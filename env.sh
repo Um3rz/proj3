c1dd4fcc699355e6c9927aeb69e920466baeb18a
Umar Zubair <61655258+Um3rz@users.noreply.github.com>
2024-12-01 17:03:26 +0500

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 # set this to your wallet's directory
-export TNS_ADMIN=/proj3/application/wallet
+export TNS_ADMIN=
 
 # provide your credentials here
-export DB_USERNAME=s26100355
-export DB_PASSWORD=c9!sQnuDVMCJvZn
-export DB_ALIAS=atp1proj0_high
+export DB_USERNAME=
+export DB_PASSWORD=
+export DB_ALIAS=
